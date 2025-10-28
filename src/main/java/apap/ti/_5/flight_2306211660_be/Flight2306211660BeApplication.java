faf83cd4e2f86ce4e7e026eab346bfd33982e6a0
Clarissa Indriana Pramesti <clarissa.indriana@ui.ac.id>
2025-10-28 10:16:57 +0700

    feat: initialized project

---
+++ src/main/java/apap/ti/_5/flight_2306211660_be/Flight2306211660BeApplication.java
@@ -0,0 +1,13 @@
+package apap.ti._5.flight_2306211660_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flight2306211660BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Flight2306211660BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apap/ti/_5/flight_2306211660_be/Flight2306211660BeApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti._5.flight_2306211660_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Flight2306211660BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
