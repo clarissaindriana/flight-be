fdb99080e86e6e7c887ba271792d490e72fd0a0a
Clarissa Indriana Pramesti <clarissa.indriana@ui.ac.id>
2025-11-27 19:01:26 +0700

    feat: fixed cors config

--- src/main/java/apap/ti/_5/flight_2306211660_be/config/CorsConfig.java
+++ src/main/java/apap/ti/_5/flight_2306211660_be/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${CORS_ALLOWED_ORIGINS}")
+    @Value("${CORS_ALLOWED_ORIGINS:http://2306211660-be.hafizmuh.site,http://2306211660-fe.hafizmuh.site,http://2306212083-be.hafizmuh.site,http://2306212083-fe.hafizmuh.site,http://2306203236-be.hafizmuh.site,http://2306203236-fe.hafizmuh.site,http://2306240061-be.hafizmuh.site,http://2306240061-fe.hafizmuh.site,http://2306219575-be.hafizmuh.site,http://2306219575-fe.hafizmuh.site,http://localhost:5173}")
     private String allowedOrigins;
 
     @Bean
@@ -18,6 +18,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 String[] origins = allowedOrigins.split(",");
+                
+                // Trim whitespace from each origin
+                for (int i = 0; i < origins.length; i++) {
+                    origins[i] = origins[i].trim();
+                }
 
                 registry.addMapping("/**")
                         .allowedOrigins(origins)
