19e085edbfaa64d020f7aaa511acc6504c95bc4d
Clarissa Indriana Pramesti <clarissa.indriana@ui.ac.id>
2025-11-30 18:36:31 +0700

    feat: built clean

--- src/main/java/apap/ti/_5/flight_2306211660_be/restcontroller/debug/DebugController.java
+++ src/main/java/apap/ti/_5/flight_2306211660_be/restcontroller/debug/DebugController.java
@@ -1,9 +1,9 @@
 package apap.ti._5.flight_2306211660_be.restcontroller.debug;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class DebugController {
@@ -12,7 +12,7 @@ public class DebugController {
     public Map<String, String> getVersion() {
         // Change this value each time you want to confirm a new deploy
         return Map.of(
-            "versionMarker", "bill-v5-api-key-attached"
+            "versionMarker", "bill-v6"
         );
     }
 }
